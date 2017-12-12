2310f8f1f7298350377060227936aa8416698a87
Bramus <bramus@bram.us>
2017-12-12 09:52:15 +0100

    🚨 Fix lint errors

--- src/MapViewDirections.js
+++ src/MapViewDirections.js
@@ -38,14 +38,14 @@ class MapViewDirections extends Component {
 			do a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5; while (a >= 32);
 			n = 1 & i ? ~(i >> 1) : i >> 1, h = i = 0;
 			do a = t.charCodeAt(u++) - 63, i |= (31 & a) << h, h += 5; while (a >= 32);
-			o = 1 & i ? ~(i >> 1) : i >> 1, l += n, r += o, d.push([l / c, r / c])
+			o = 1 & i ? ~(i >> 1) : i >> 1, l += n, r += o, d.push([l / c, r / c]);
 		}
 
 		return d = d.map(function(t) {
 			return {
 				latitude: t[0],
 				longitude: t[1],
-			}
+			};
 		});
 	}
 
