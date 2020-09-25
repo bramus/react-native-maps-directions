e50312eb7ba71882191eef3c7e598fe96e979d50
mam17 <murilo.medeiros.gyn@gmail.com>
2020-09-25 10:43:45 -0300

    #46 Removing unused code

--- src/MapViewDirections.js
+++ src/MapViewDirections.js
@@ -277,7 +277,6 @@ class MapViewDirections extends Component {
 									];
 								}, [])
 						),
-						steps: route.legs,
 						fare: route.fare,
 						waypointOrder: route.waypoint_order,
 					});
